1dc474ac984f6964a70ba5a9eadd22abc31d56b4
Mike Jans <mike@jans.li>
2021-01-23 18:20:51 +0000

    adjust decimal points

--- script.js
+++ script.js
@@ -27,14 +27,6 @@ xmlhttp.send();
 xmlhttp2.open("GET", url2, true);
 xmlhttp2.send();
 
-/*
-  parseJson in beiden onreadystatechange Funktionen aufrufen
-  Die JSON-Daten werden asynchron bezogen. D.h. du weisst nicht wann die Daten wirklich da sind.
-  Wenn man nun die Funktion bei beiden Datenbezügen aufruft und an erster Stelle in der
-  Funktion prüft ob beide Datensätze da sind, kann man das "synchronisieren" oder wie
-  man das auch immer nennen will.
-*/
-
 function parseJson() {
   if (json == undefined || json2 == undefined) {
     return;
@@ -54,28 +46,28 @@ function parseJson() {
   document.getElementById("data").innerHTML = time +
 
     "<br /><br /><b> Bitcoin USD Values</b>" +
-    "<br />" + "1 BTC equals to USD: " + usdValue +
-    "<br />" + "0.1 BTC equals to USD: " + (usdValue / 10) +
-    "<br />" + "1.554 BTC equals to USD: " + (usdValue * 1.554) +
-    "<br />" + "1 USD is in satoshis: " + satusd +
-    "<br />" + "1 satoshi is in USD: " + (usdValue / 100000000) +
-    "<br /><br />" +
+    "<br />" + "1 BTC equals to USD: " + usdValue.toFixed(2) +
+    "<br />" + "0.1 BTC equals to USD: " + (usdValue / 10).toFixed(2) +
+    "<br />" + "1.554 BTC equals to USD: " + (usdValue * 1.554).toFixed(2) +
+    "<br />" + "1 USD is in satoshis: " + satusd.toFixed(2) +
+    "<br />" + "1 satoshi is in USD: " + (usdValue / 100000000).toFixed(6) +
+
     "<br /><br /> <b>Bitcoin CHF Values</b>" +
-    "<br />" + "1 BTC equals to CHF: " + (usdValue * usdchf) +
-    "<br />" + "0.1 BTC equals to CHF: " + ((usdValue * usdchf / 10)) +
-    "<br />" + "1.554 BTC equals to CHF: " + ((usdValue * usdchf * 1.554)) +
-    "<br />" + "1 CHF is in satoshis: " + satchf +
-    "<br />" + "1 satoshi is in CHF: " + (usdValue * usdchf / 100000000) +
-    "<br /><br />" +
+    "<br />" + "1 BTC equals to CHF: " + (usdValue * usdchf).toFixed(2) +
+    "<br />" + "0.1 BTC equals to CHF: " + ((usdValue * usdchf / 10)).toFixed(2) +
+    "<br />" + "1.554 BTC equals to CHF: " + ((usdValue * usdchf * 1.554)).toFixed(2) +
+    "<br />" + "1 CHF is in satoshis: " + satchf.toFixed(2) +
+    "<br />" + "1 satoshi is in CHF: " + (usdValue * usdchf / 100000000).toFixed(6) +
+
     "<br /><br /> <b>Bitcoin THB Values</b>" +
-    "<br />" + "1 BTC equals to THB: " + (usdValue * usdthb) +
-    "<br />" + "0.1 BTC equals to THB: " + ((usdValue * usdthb / 10)) +
-    "<br />" + "1.554 BTC equals to THB: " + ((usdValue * usdthb * 1.554)) +
-    "<br />" + "1 THB is in satoshis: " + satthb +
-    "<br />" + "1 satoshi is in THB: " + (usdValue * usdthb / 100000000) +
-    "<br /><br />" +
+    "<br />" + "1 BTC equals to THB: " + (usdValue * usdthb).toFixed(2) +
+    "<br />" + "0.1 BTC equals to THB: " + ((usdValue * usdthb / 10)).toFixed(2) +
+    "<br />" + "1.554 BTC equals to THB: " + ((usdValue * usdthb * 1.554)).toFixed(2) +
+    "<br />" + "1 THB is in satoshis: " + satthb.toFixed(2) +
+    "<br />" + "1 satoshi is in THB: " + (usdValue * usdthb / 100000000).toFixed(6) +
+
     "<br /><br /> <b>CHF THB conversion</b>" +
-    "<br />" + "1 CHF equals to THB: " + (thbchf) +
-    "<br /><br />";
+    "<br />" + "1 CHF equals to THB: " + (thbchf.toFixed(2)) +
+    "";
 }
 
